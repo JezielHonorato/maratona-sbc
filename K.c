bd6f62b0c9ca5c11669eb4595b874bffb8897879
JezielHonorato <136933391+JezielHonorato@users.noreply.github.com>
2026-09-16 22:04:47 -0300

    Update K.c

--- K.c
+++ K.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int n, soma = 0, menor_diferenca;
 	scanf("%d", &n);
-	
+
 	int produtos[n];
 	int metas[n];
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		scanf("%d", &produtos[i]);
 		soma = soma + produtos[i];
 	}
 
 	scanf("%d", &metas[0]);
-	if (metas[0] > produtos[0]) {
+	if (metas[0] > produtos[0])
+	{
 		printf("%d", -1);
 		return 0;
-	} 
+	}
 	menor_diferenca = produtos[0] - metas[0];
 
-	for (int i = 1; i < n; i++) {
+	for (int i = 1; i < n; i++)
+	{
 		scanf("%d", &metas[i]);
-		if (metas[i] > produtos[i]) {
+		if (metas[i] > produtos[i])
+		{
 			printf("%d", -1);
 			return 0;
 		}
-		if (menor_diferenca > produtos[i] - metas[i]) {
+		if (menor_diferenca > produtos[i] - metas[i])
+		{
 			menor_diferenca = produtos[i] - metas[i];
 		}
 	}
